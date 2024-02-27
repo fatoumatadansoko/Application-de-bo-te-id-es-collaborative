ad966dc2adfdd0136b3c023391455cd501f84dcc
fatoumata dansoko <fatoumatadansoko61@gmail.com>
2024-02-27 13:15:16 +0000

    Ajout des différentes images et logo du site et mis en place de la page index.php,inscription.php et style.css

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>InnovateTech</title>
+</head>
+<body>
+
+<h1>InnovateTech</h1>
+         <div class="head">
+    <img src="img/design.png" alt="">
+
+    <a href="inscription.php"> Sinscrire</a>
+    </div>
+    
+</body>
+</html>
